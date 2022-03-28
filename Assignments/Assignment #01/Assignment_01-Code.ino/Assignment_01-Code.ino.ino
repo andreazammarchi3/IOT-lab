a4e736e5c9f92fbb8b2bae06d9c9428699afc9b1
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-03-28 17:23:42 +0200

    Update Assignment_01-Code.ino.ino

--- Assignments/Assignment #01/Assignment_01-Code.ino/Assignment_01-Code.ino.ino
+++ Assignments/Assignment #01/Assignment_01-Code.ino/Assignment_01-Code.ino.ino
@@ -1,8 +1,19 @@
+/*
+ * Assignment #01 - Catch the Bouncing Led Ball
+ * 
+ * Author: andrea.zammarchi3@studio.unibo.it - 0000914652
+ * 
+ * Link repo GitHub:
+ * https://github.com/andreazammarchi3/IOT-lab/tree/main/Assignments/Assignment%20%2301
+ */
+
+/* Include necessary libraries. */
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include "EnableInterrupt.h"
 #include "TimerOne.h"
 
+/* Define names for pins. */
 #define Pot A0
 
 #define T1 8
@@ -16,6 +27,7 @@
 #define L3 5
 #define L4 3
 
+/* Define global vars. */
 char Leds[] = {L1, L2, L3, L4};
 char Btns[] = {T1, T2, T3, T4};
 
@@ -26,35 +38,25 @@ int brightness;
 int fadeAmount;
 int score;
 int appStatus;
-boolean forward;
-
 int S;
 int P;
+
+boolean forward;
+
 float t1;
 float t1_counter;
 float t2;
 float F;
 float lvlFactor;
 
-void resetVarsToDefaultValue() {
-  sleepCounter = 0;
-  sleepCountdown = 10000; /* 10000 msec = 10 sec */
-  fadingSpeed = 20;
-  brightness = 0;
-  fadeAmount = 5;
-  score = 0;
-  forward = true;
-  S = 1000;
-  F = 1;
-  P = 1;
-  t1_counter = 0;
-  t2 = 10000;
-}
 
+/*
+ * Setup method, first method to be executed from Arduino.
+ */
 void setup() {
   Serial.begin(9600);
   for (int i = 0; i < sizeof(Btns); i++) {
-    pinMode(Btns[i], INPUT_PULLUP);
+    pinMode(Btns[i], INPUT);
   }
   pinMode(Ls, OUTPUT);
   for (int i = 0; i < sizeof(Leds); i++) {
@@ -64,10 +66,16 @@ void setup() {
   initialState();
 }
 
+/*
+ * Super-loop, main app method.
+ */
 void loop() {
   switch (appStatus) {
-    case 1: /* Initial state */
-      // Waiting T1 to be clicked. If not, after 10s go to sleep.
+    case 1:
+      /* 
+       * Initial state:
+       * Waiting T1 to be clicked. If not, after 10s go to sleep.
+       */
       analogWrite(Ls, brightness);
       brightness = brightness + fadeAmount;
       if (brightness <= 0 || brightness >= 255) {
@@ -82,8 +90,11 @@ void loop() {
       sleepCounter++;
       break;
 
-    case 2: /* RunBall state */
-      // Ball is traveling through the leds
+    case 2:
+      /* 
+       * RunBall state:
+       * Ball is shifting through the leds, after random time it stops.
+       */
       if (forward == true) {
         for (P = 1; P <= 3; P++) {
           if(P != 1) {
@@ -119,8 +130,11 @@ void loop() {
       }
       break;
 
-    case 3: /* CatchBall state */
-      // User must press the correct button to increase the score.
+    case 3:
+      /* 
+       * CatchBall state:
+       * User must press the correct button to increase the score.
+       */
       enableInterrupt(Btns[P-1], incScore, RISING);
       delay(t2 / F);
       disableInterrupt(Btns[P-1]);
@@ -130,8 +144,11 @@ void loop() {
       digitalWrite(Leds[P-1], LOW);
       break;
 
-    case 4: /* GameOver state */
-      // Game is over. Go back to initial state.
+    case 4:
+      /* 
+       * GameOver state:
+       * Game is over. Go back to initial state.
+       */
       String str = "Game Over. Final Score: ";
       str += score;
       Serial.println(str);
@@ -141,17 +158,45 @@ void loop() {
       break;
 
     default:
+      /* 
+       * Theoretically it shouldn't happen.
+       * In case of bugs, go back to the initial state.
+       */
       appStatus = 1;
       initialState();
       break;
   }
 }
 
+/*
+ * Reset some vars to their default values.
+ */
+void resetVarsToDefaultValue() {
+  sleepCounter = 0;
+  sleepCountdown = 10000; /* 10000 msec = 10 sec */
+  fadingSpeed = 20;
+  brightness = 0;
+  fadeAmount = 5;
+  score = 0;
+  forward = true;
+  S = 1000;
+  F = 1;
+  P = 1;
+  t1_counter = 0;
+  t2 = 10000;
+}
+
+/*
+ * Generate a random integer in a range.
+ */
 void genRandom() {
   randomSeed(analogRead(5));
   t1 = random(5, 15);
 }
 
+/*
+ * Few firts instructions to do when app goes to initial state.
+ */
 void initialState() {
   resetVarsToDefaultValue();
   Serial.println("Welcome to the Catch the Bouncing Led Ball Game. Press Key T1 to Start");
@@ -162,6 +207,9 @@ void initialState() {
   enableInterrupt(T1, startGame, RISING);
 }
 
+/*
+ * Method called when Arduino has to enter in Deep Sleep Mode.
+ */
 void startSleep() {
   disableInterrupt(T1);
   for (int i = 0; i < sizeof(Btns); i++) {
@@ -176,6 +224,9 @@ void startSleep() {
   sleep_disable();
 }
 
+/*
+ * Method called when Arduino exits from Deep Sleep Mode.
+ */
 void wakeUp() {
   for (int i = 0; i < sizeof(Btns); i++) {
     disableInterrupt(Btns[i]);
@@ -184,6 +235,9 @@ void wakeUp() {
   initialState();
 }
 
+/*
+ * Method called by interrupt on T1. The game starts.
+ */
 void startGame() {
   disableInterrupt(T1);
   int potVal = analogRead(Pot);
@@ -194,6 +248,10 @@ void startGame() {
   appStatus = 2;
 }
 
+/*
+ * Method called on interrupt on the corresponding btn in game.
+ * Increment current score.
+ */
 void incScore() {
   disableInterrupt(Btns[P-1]);
   score++;
