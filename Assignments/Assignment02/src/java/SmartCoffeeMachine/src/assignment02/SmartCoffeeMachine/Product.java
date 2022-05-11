946bfa54fd5366930c91f3d6549c9d1cb803f3d3
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-05-11 17:30:12 +0200

    Update Product.java

--- Assignments/Assignment02/src/java/SmartCoffeeMachine/src/assignment02/SmartCoffeeMachine/Product.java
+++ Assignments/Assignment02/src/java/SmartCoffeeMachine/src/assignment02/SmartCoffeeMachine/Product.java
@@ -1,43 +1,90 @@
 package assignment02.SmartCoffeeMachine;
 
+/**
+ * Product class, implemented Coffe, Tea and Chocolate
+ */
 public class Product {
+    /**
+     * Name of the product
+     */
     private String label;
+
+    /**
+     * Quantity available for this product
+     */
     private int availability;
 
+    /**
+     * Max availability for this product
+     */
     private int maxAvailability;
 
+    /**
+     * Constructor
+     * @param label Name of the product
+     * @param availability Quantity available
+     * @param maxAvailability Max availability
+     */
     public Product(String label, int availability, int maxAvailability) {
         this.label = label;
         this.availability = availability;
         this.maxAvailability = maxAvailability;
     }
 
+    /**
+     * Constructor
+     * @param label Name of the product
+     */
     public Product(String label) {
         this.label = label;
         this.availability = 0;
         this.maxAvailability = 0;
     }
 
+    /**
+     * Set product name
+     * @param label Name
+     */
     public void setLabel(String label) {
         this.label = label;
     }
 
+    /**
+     * Set quantity left
+     * @param availability Quantity left
+     */
     public void setAvailability(int availability) {
         this.availability = availability;
     }
 
+    /**
+     * Set max availability
+     * @param maxAvailability Max availability
+     */
     public void setMaxAvailability(int maxAvailability) {
         this.maxAvailability = maxAvailability;
     }
 
+    /**
+     * Get product name
+     * @return Product name
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Get product availability
+     * @return Product availability
+     */
     public int getAvailability() {
         return availability;
     }
 
+    /**
+     * Get product max availability
+     * @return Product max availability
+     */
     public int getMaxAvailability() {
         return maxAvailability;
     }
