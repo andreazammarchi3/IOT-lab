996652b215cea82e31af2670dbd222279c95b759
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-07-05 14:54:59 +0200

    Create basic app layout

--- Assignments/Assignment03/garden-app/app/src/main/java/com/example/gardenapp/MainActivity.java
+++ Assignments/Assignment03/garden-app/app/src/main/java/com/example/gardenapp/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.gardenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mButton = findViewById(R.id.button_require_manual_control);
+        mButton.setOnClickListener(view -> System.out.println("clicked"));
     }
 }
