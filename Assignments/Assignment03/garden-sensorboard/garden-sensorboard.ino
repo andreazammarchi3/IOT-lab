7c92b4b5b2a2010ea0d4d8930ffbd41c90b8c2ec
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-07-26 16:54:25 +0200

    Remove py files and update sensorboard

--- Assignments/Assignment03/garden-sensorboard/garden-sensorboard.ino
+++ Assignments/Assignment03/garden-sensorboard/garden-sensorboard.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
-#include <HTTPClient.h>
+#include <PubSubClient.h>
+#define MSG_BUFFER_SIZE  50
 
 //variabili globali
 int tempVal = 0;
@@ -11,21 +12,97 @@ const int tempPin = 35;
 const int ledPin = 23;
 const int photoPin = 34;
 
-const char* ssid = "iPhone di Andrea";
-const char* password =  "infondoalmar";
+/* wifi network info */
 
-const char *serverPath = "https://3419-37-160-47-90.eu.ngrok.io";
+const char* ssid = "AIR2";
+const char* password = "giovanniboss";
 
-void connectToWifi(const char* ssid, const char* password){
+/* MQTT server address */
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+
+/* MQTT client management */
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsgTime = 0;
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+
+  Serial.println(String("Connecting to ") + ssid);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+/* MQTT subscribing callback */
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+  /*
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+    */
+}
+
+void reconnect() {
+  
+  // Loop until we're reconnected
+  
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    // Create a random client ID
+    String clientId = String("SmartGarden-client-")+String(random(0xffff), HEX);
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("SmartGarden/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 float getTemp() {
@@ -47,39 +124,38 @@ void setup()
   
   digitalWrite(ledPin, HIGH);
 
-  Serial.print("Temperature: ");
-  Serial.println(getTemp());
-  Serial.print("Light: ");
-  Serial.println(getLight());
-
   Serial.begin(115200);
-  delay(4000);
-  connectToWifi(ssid, password);
+  delay(2000);
+  setup_wifi();
+  randomSeed(micros());
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop()
 { 
-  if(WiFi.status()== WL_CONNECTED){      
-    HTTPClient http;
-  
-    // Your Domain name with URL path or IP address with path
-    http.begin(serverPath);
-      
-    // Send HTTP GET request
-    int httpResponseCode = http.GET();
-      
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-    
-    // Free resources
-    http.end();
-    }
-   delay(2000);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsgTime > 10000) {
+    lastMsgTime = now;
+    value++;
+
+    /* Publishing the temperature */
+    char tempString[8];
+    dtostrf(getTemp(), 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    client.publish("SmartGarden/temperature", tempString);
+
+    /* Publishing the luminosity*/
+    char lightString[8];
+    dtostrf(getLight(), 1, 2, lightString);
+    Serial.print("Luminosity: ");
+    Serial.println(lightString);
+    client.publish("SmartGarden/luminosity", lightString);
+  }
 }
