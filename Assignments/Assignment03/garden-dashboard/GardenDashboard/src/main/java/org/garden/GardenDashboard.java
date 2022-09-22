ed86093edb726039186b74fc9530f44a6c39fdbc
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-09-22 16:19:13 +0200

    Update GardenDashboard.java

--- Assignments/Assignment03/garden-dashboard/GardenDashboard/src/main/java/org/garden/GardenDashboard.java
+++ Assignments/Assignment03/garden-dashboard/GardenDashboard/src/main/java/org/garden/GardenDashboard.java
@@ -1,7 +1,25 @@
 package org.garden;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class GardenDashboard {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        // Create client socket
+        Socket s = new Socket("localhost", 888);
+
+        // to read data coming from the server
+
+        BufferedReader br
+                = new BufferedReader(
+                new InputStreamReader(
+                        s.getInputStream()));
+
+        String inputStr = br.readLine();
+        System.out.println(inputStr);
+
+        br.close();
+        s.close();
     }
 }
