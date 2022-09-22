0f12453d7c45d5fa55fc519e8fc37a33fa91f66f
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-09-22 16:23:50 +0200

    Refactor gradle settings GardenService

---
+++ Assignments/Assignment03/garden-service/GardenService/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "org.garden"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+application {
+    mainClass.set("org.garden.GardenService")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ Assignments/Assignment03/garden-service/GardenService/src/main/java/org/garden/GardenService.java
@@ -0,0 +1,7 @@
+package org.garden;
+
+public class GardenService {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
