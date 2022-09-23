8ebc503d7520cf01ebcba100f2469c9209854f12
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-09-23 16:19:01 +0200

    Service and Dashboard can communicate

--- Assignments/Assignment03/garden-dashboard/GardenDashboard/src/main/java/org/garden/GardenDashboard.java
+++ Assignments/Assignment03/garden-dashboard/GardenDashboard/src/main/java/org/garden/GardenDashboard.java
@@ -1,33 +1,38 @@
 package org.garden;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Objects;
 
 public class GardenDashboard {
     public static void main(String[] args) throws Exception {
-        /*
-        // Create client socket
+        // Initialize client socket
         Socket s = new Socket("localhost", 888);
 
         // to read data coming from the server
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-        BufferedReader br
-                = new BufferedReader(
-                new InputStreamReader(
-                        s.getInputStream()));
+        // Initialize GUI
+        GardenDashboardGUI gui = new GardenDashboardGUI();
 
-        String inputStr = br.readLine();
-        System.out.println(inputStr);
+        String inputStr = "";
+        while(!Objects.equals(inputStr, "close")) {
+            long millis = System.currentTimeMillis();
+
+            inputStr = br.readLine();
+            if (inputStr.contains("l")) {
+                int l = Integer.parseInt(inputStr.replace("l: ",""));
+                gui.setValueL(l);
+            } else {
+                int t = Integer.parseInt(inputStr.replace("t: ",""));
+                gui.setValueT(t);
+            }
+
+            Thread.sleep(1000 - millis % 1000);
+        }
 
         br.close();
         s.close();
-         */
-
-        GardenDashboardGUI gui = new GardenDashboardGUI();
-        gui.setValueL(8);
-        gui.setValueT(5);
     }
 }
--- Assignments/Assignment03/garden-service/GardenService/src/main/java/org/garden/GardenService.java
+++ Assignments/Assignment03/garden-service/GardenService/src/main/java/org/garden/GardenService.java
@@ -1,7 +1,50 @@
 package org.garden;
 
+import java.io.*;
+import java.net.*;
+
 public class GardenService {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // create server
+        ServerSocket ss = new ServerSocket(888);
+
+        // connect it to client socket
+        Socket s = ss.accept();
+        System.out.println("Connection to the client socket established");
+
+        // to send data to the client
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        int l = 8;
+        int t = 5;
+        boolean even = false;
+        String msg = "";
+
+        while(s.isConnected()) {
+            long millis = System.currentTimeMillis();
+
+            try {
+                if (even) {
+                    msg = "l: " + l;
+                    even = false;
+                    l++;
+                } else {
+                    even = true;
+                    msg = "t: " + t;
+                    t++;
+                }
+                ps.println(msg);
+            } catch (Exception e) {
+                System.out.println("ERROR");
+                ps.println("ERROR");
+            }
+
+            Thread.sleep(1000 - millis % 1000);
+        }
+
+        ps.close();
+        ss.close();
+        s.close();
+        System.out.println("Server and client CLOSED");
     }
 }
