6bc233195c1ef14acd301834f083a09e0b390ece
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-10-05 12:11:07 +0200

    Update Android App

--- Assignments/Assignment03/garden-app/SmartGarden/app/src/main/java/com/example/smartgarden/MainActivity.java
+++ Assignments/Assignment03/garden-app/SmartGarden/app/src/main/java/com/example/smartgarden/MainActivity.java
@@ -1,12 +1,29 @@
 package com.example.smartgarden;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean led1Bool = false;
+    public boolean led2Bool = false;
+    public int led3Counter = 0;
+    public int led4Counter = 0;
+    public String mode = "AUTO";
+    public boolean irrMode = false;
+    public int irrCounter = 0;
+
+    private TextView led3CounterText = findViewById(R.id.led_3_counter);
+
+    private TextView led4CounterText = findViewById(R.id.led_4_counter);
+
+    private TextView irrCounterText = findViewById(R.id.irrigation_counter);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,58 +33,86 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonRequireManualControl = findViewById(R.id.button_require_manual_control);
         buttonRequireManualControl.setOnClickListener(view ->
-                System.out.println("clicked")
+                System.out.println("Required MANUAL control")
         );
 
         Button buttonLed1 = findViewById(R.id.button_led_1);
         buttonLed1.setOnClickListener(view ->
-                System.out.println("clicked")
+                boolBtnClick(led1Bool)
         );
 
         Button buttonLed2 = findViewById(R.id.button_led_2);
         buttonLed2.setOnClickListener(view ->
-                System.out.println("clicked")
+                boolBtnClick(led2Bool)
         );
 
         Button buttonLed3Plus = findViewById(R.id.button_led_3_plus);
         buttonLed3Plus.setOnClickListener(view ->
-                System.out.println("clicked")
+                counterBtnClick(led3Counter, led3CounterText, true)
         );
 
         Button buttonLed3Minus = findViewById(R.id.button_led_3_minus);
         buttonLed3Minus.setOnClickListener(view ->
-                System.out.println("clicked")
+                counterBtnClick(led3Counter, led3CounterText, false)
         );
 
         Button buttonLed4Plus = findViewById(R.id.button_led_4_plus);
         buttonLed4Plus.setOnClickListener(view ->
-                System.out.println("clicked")
+                counterBtnClick(led4Counter, led4CounterText, true)
         );
 
         Button buttonLed4Minus = findViewById(R.id.button_led_4_minus);
         buttonLed4Minus.setOnClickListener(view ->
-                System.out.println("clicked")
+                counterBtnClick(led4Counter, led4CounterText, false)
         );
 
         Button buttonIrrigationMinus = findViewById(R.id.button_irrigation_minus);
         buttonIrrigationMinus.setOnClickListener(view ->
-                System.out.println("clicked")
+                counterBtnClick(irrCounter, irrCounterText, false)
         );
 
         Button buttonIrrigationPlus = findViewById(R.id.button_irrigation_plus);
         buttonIrrigationPlus.setOnClickListener(view ->
-                System.out.println("clicked")
+                counterBtnClick(irrCounter, irrCounterText, true)
         );
 
         Button buttonIrrigation = findViewById(R.id.button_irrigation);
         buttonIrrigation.setOnClickListener(view ->
-                System.out.println("clicked")
+                boolBtnClick(irrMode)
         );
+    }
 
-        TextView led3Counter = findViewById(R.id.led_3_counter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        TextView led4Counter = findViewById(R.id.led_4_counter);
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_alarm) {
+            System.out.println("Alarm btn clicked");
+        }
+        return true;
+    }
 
-        TextView irrigationCounter = findViewById(R.id.irrigation_counter);
+    private void boolBtnClick(boolean bool) {
+        bool = !bool;
+    }
+
+    private void counterBtnClick(int counter, TextView view, boolean sum) {
+        if(counter<4 && sum) {
+            counter++;
+        } else if(counter>0 && !sum) {
+            counter--;
+        }
+        view.setText(counter);
+    }
+
+    private void led4PlusClick() {
+        if(led4Counter<4) {
+            led4Counter++;
+        }
+        led4CounterText.setText(led4Counter);
     }
 }
